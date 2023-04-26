440973e70e80eebdf4ac2865f4cbb8b72ddddf5c
Ravi Prakash <72187399+Ravi24242424@users.noreply.github.com>
2023-04-26 13:39:57 +0530

    Update application.py

--- application.py
+++ application.py
@@ -29,6 +29,9 @@ def predict_datapoint():
         ISI = float(request.form.get('ISI'))
         Classes = float(request.form.get('Classes'))
         Region = float(request.form.get('Region'))
+        
+        new_data_scaled= scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge.predict(new_data_scaled)
 
 
         return render_template('home.html',result=result[0])
