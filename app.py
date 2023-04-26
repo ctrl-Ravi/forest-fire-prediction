c7a190450dcd57132ca525bd5895fc26cbe3c1d4
Ravi24242424 <rp727906@gmail.com>
2023-04-26 12:29:55 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
+import pickle
+from flask import Flask,request,jsonify,render_template
 from flask import Flask
-
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+## import ridge regresor model and standard scaler pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler1.pkl','rb'))
+
+
+### Route for how page
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method =='POST':
+        Temperature=int(request.form.get('Temperature'))
+        RH = int(request.form.get('RH'))
+        Ws = int(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+
+        return render_template('home.html',result=result[0])
+
+    else:
+        return render_template('home.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
